4460bb7408ce0d577cbd2fd609bad6b020292e3b
Abdullah Mutlu <abdullah.mutllu@gmail.com>
2024-11-28 20:23:44 +0300

    default page

---
+++ final-client/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <Navbar
+     expand="lg"
+     background-color="body-tertiary"
+    >
+     <Container type="fluid">
+      <NavbarBrand>Navbar w/ text</NavbarBrand>
+      <NavbarToggler />
+      <NavbarCollapse>
+       <NavbarNavList margin="e-auto b-2 b-lg-0">
+        <NavItem>
+         <NavLink active>
+          Home
+         </NavLink>
+        </NavItem>
+        <NavItem>
+         <NavLink>Features</NavLink>
+        </NavItem>
+        <NavItem>
+         <NavLink>Pricing</NavLink>
+        </NavItem>
+       </NavbarNavList>
+       <NavbarText>Navbar text with an inline element</NavbarText>
+      </NavbarCollapse>
+     </Container>
+    </Navbar>
+   </template>
---
+++ final-client/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <Navbar  @toggle-sidebar="toggleSidebar" />
+        <Sidebar/>
+        <main :class="{'content-expanded':sidebarVisible }">
+            <slot/>
+        </main>
+    </div>
+</template>
+<script setup>
+const sidebarVisible = ref(false);
+const toggleSidebar = () => {
+  sidebarVisible.value = !sidebarVisible.value;
+};
+</script>
